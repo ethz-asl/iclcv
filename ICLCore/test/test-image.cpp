a105153568701b7546900afabec4f7b9c1318c68
celbrech <celbrech@7f3f178b-3016-0410-991d-991d90baa052>
2013-03-18 20:42:30 +0000

    moved the cmake_revise_branch to the trunk

--- ICLCore/test/test-image.cpp
+++ ICLCore/test/test-image.cpp
@@ -1,7 +1,7 @@
 /********************************************************************
 **                Image Component Library (ICL)                    **
 **                                                                 **
-** Copyright (C) 2006-2012 CITEC, University of Bielefeld          **
+** Copyright (C) 2006-2013 CITEC, University of Bielefeld          **
 **                         Neuroinformatics Group                  **
 ** Website: www.iclcv.org and                                      **
 **          http://opensource.cit-ec.de/projects/icl               **
@@ -11,18 +11,14 @@
 ** Authors: Christof Elbrechter                                    **
 **                                                                 **
 **                                                                 **
-** Commercial License                                              **
-** ICL can be used commercially, please refer to our website       **
-** www.iclcv.org for more details.                                 **
+** GNU LESSER GENERAL PUBLIC LICENSE                               **
+** This file may be used under the terms of the GNU Lesser General **
+** Public License version 3.0 as published by the                  **
 **                                                                 **
-** GNU General Public License Usage                                **
-** Alternatively, this file may be used under the terms of the     **
-** GNU General Public License version 3.0 as published by the      **
 ** Free Software Foundation and appearing in the file LICENSE.GPL  **
 ** included in the packaging of this file.  Please review the      **
-** following information to ensure the GNU General Public License  **
-** version 3.0 requirements will be met:                           **
-** http://www.gnu.org/copyleft/gpl.html.                           **
+** following information to ensure the license requirements will   **
+** be met: http://www.gnu.org/licenses/lgpl-3.0.txt                **
 **                                                                 **
 ** The development of this software was supported by the           **
 ** Excellence Cluster EXC 277 Cognitive Interaction Technology.    **
@@ -30,7 +26,7 @@
 ** Forschungsgemeinschaft (DFG) in the context of the German       **
 ** Excellence Initiative.                                          **
 **                                                                 **
-*********************************************************************/
+********************************************************************/
 
 #include <gtest/gtest.h>
 
