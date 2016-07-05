0cfdfa9f3db5286da26d3218eb0a19f0816d9447
Fadri Furrer <ffurrer@gmail.com>
2016-07-05 10:37:35 +0200

    first part to add depth16u type

--- ICLFilter/src/ICLFilter/WienerOp.cpp
+++ ICLFilter/src/ICLFilter/WienerOp.cpp
@@ -36,53 +36,54 @@ using namespace icl::core;
 
 namespace icl {
   namespace filter{
-  
-    
+
+
   #ifdef ICL_HAVE_IPP
-    
+
     namespace{
       template<typename T, IppStatus (IPP_DECL *ippiFunc) (const T*, int, T*, int, IppiSize, IppiSize, IppiPoint, float[], icl8u*)>
-      void ippiWienerCall (const Img<T> *src, 
+      void ippiWienerCall (const Img<T> *src,
                            Img<T> *dst,
-                           const Size &oMaskSize, 
+                           const Size &oMaskSize,
                            const Point &oAnchor,
                            const Point &roiOffset,
-                           std::vector<icl8u> &vBuffer, 
+                           std::vector<icl8u> &vBuffer,
                            icl32f fNoise) {
         // {{{ open
         int iBufferSize;
         ippiFilterWienerGetBufferSize(dst->getROISize(), oMaskSize, 1, &iBufferSize);
         vBuffer.reserve (iBufferSize);
-        
+
         for(int c=src->getChannels()-1; c>=0; --c) {
           ippiFunc(src->getROIData (c, roiOffset), src->getLineStep(),
                    dst->getROIData (c), dst->getLineStep(),
                    dst->getROISize(), oMaskSize, oAnchor, &fNoise, &vBuffer[0]);
         };
       }
-    
+
   // }}}
-    } // end of anonymous namespace   
-  
-  
+    } // end of anonymous namespace
+
+
     void WienerOp::apply (const ImgBase *poSrc, ImgBase **ppoDst) {
       // {{{ open
       FUNCTION_LOG("");
       if (!prepare (ppoDst, poSrc)) return;
-      
+
       switch(poSrc->getDepth()){
   #define ARGLIST(D) poSrc->asImg<icl##D>(),(*ppoDst)->asImg<icl##D>(),getMaskSize(),getAnchor(),getROIOffset(),m_vecBuffer,m_fNoise
         case depth8u: ippiWienerCall<icl8u,ippiFilterWiener_8u_C1R>(ARGLIST(8u)); break;
         case depth16s: ippiWienerCall<icl16s,ippiFilterWiener_16s_C1R>(ARGLIST(16s)); break;
+        case depth16u: ippiWienerCall<icl16u,ippiFilterWiener_16u_C1R>(ARGLIST(16u)); break;
         case depth32f: ippiWienerCall<icl32f,ippiFilterWiener_32f_C1R>(ARGLIST(32f)); break;
         default: ICL_INVALID_DEPTH;
   #undef ARGLIST
       }
     }
-    
+
     // }}}
-  
-  
-  #endif     
+
+
+  #endif
   } // namespace filter
 }
